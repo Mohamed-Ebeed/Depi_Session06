cbcd7126b41d6911caf10906d2579cf99eaa896c
Mohamed Ahmed <ebaidmedo101@gmail.com>
2026-09-20 20:31:13 +0300

    Add HiringDate class for managing hiring dates

---
+++ HiringDate.cs
@@ -0,0 +1,28 @@
+namespace Depi_Session06
+{
+    public class HiringDate
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public HiringDate()
+        {
+            Day = 1;
+            Month = 1;
+            Year = 2000;
+        }
+
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day:D2}/{Month:D2}/{Year}";
+        }
+    }
+}
